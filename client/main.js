89ac0d68d6beb945fcdd2514e87c427b2d87f826
gitMonks <thanksimran@gmail.com>
2017-08-01 09:30:17 +0100

    Setuping git, first commit

---
+++ client/main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+
+import Header from './components/header';
+import LinkCreate from './components/link_create';
+import LinkList from './components/link_list';
+
+import { Links } from '../imports/collections/links';
+
+const App = () => {
+	return (
+		<div>
+			<Header />
+			<LinkCreate />
+			<LinkList />
+		</div>
+	)
+}
+
+Meteor.startup(() => {
+	ReactDOM.render(<App />, document.querySelector('#render-container'))
+});
